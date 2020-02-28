2d84bcef76ea8ed6241efafcf1cf0ba725a38789
Yayun Yang <hxjkim92@gmail.com>
2020-02-27 19:14:25 -0500

    User Validation Updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,17 @@ class EmailValidator < ActiveModel::EachValidator
     end
 end
 
+#Email Regex can be added in 2 ways
+#1. Email validator + email: true
+#2. VALID_EMAIL_REGEX + format: {with: VALID_EMAIL_REGEX}
+
 class User < ApplicationRecord
     validates :username,  presence: true, 
                 length: { minimum: 3, maximum:25 }, 
-                uniqueness: true { case_sensitive: false }
-    #VALID_EMAIL_REGEX: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+                uniqueness: { case_sensitive: false }
+    #VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     validates :email, presence: true, 
-                uniqueness: true { case_sensitive: false },
+                uniqueness: { case_sensitive: false },
                 email: true
                 #format: {with: VALID_EMAIL_REGEX}
 

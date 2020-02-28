7dc4144b4ce03b6580295f6876439a40693f82cc
Yayun Yang <hxjkim92@gmail.com>
2020-02-27 19:08:23 -0500

    User Validation Added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
-class User < ApplicationRecord
+class EmailValidator < ActiveModel::EachValidator
+    def validate_each(record,attribute, value)
+        unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+            record.errors[attribute] << (options[:message] || "is not an email")
+        end
+    end
+end
 
+class User < ApplicationRecord
+    validates :username,  presence: true, 
+                length: { minimum: 3, maximum:25 }, 
+                uniqueness: true { case_sensitive: false }
+    #VALID_EMAIL_REGEX: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates :email, presence: true, 
+                uniqueness: true { case_sensitive: false },
+                email: true
+                #format: {with: VALID_EMAIL_REGEX}
 
 end
+

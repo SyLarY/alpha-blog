ddb22f2fd22426e39d3aa9d46d59fe3e91d02d41
Yayun Yang <hxjkim92@gmail.com>
2020-03-01 01:54:16 -0500

    Pagination to Views Added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:edit, :update, :show]
+    before_action :set_user, only: [:edit, :update]
 
     def index
-        @users = User.all
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def new
@@ -20,7 +20,8 @@ class UsersController < ApplicationController
     end
 
     def show
-
+        @user = User.find(params[:id])
+        @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def edit

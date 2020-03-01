6c5288fb2e6f91872437a79444011bb24aa07acf
Yayun (Kim) Yang <hxjkim92@gmail.com>
2020-03-01 16:41:17 -0500

    Authentication Completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
+    before_action :require_same_user, only: [:edit, :update]
 
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
@@ -40,8 +41,16 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+
     def set_user
         @user = User.find(params[:id])
+    end
+
+    def require_same_user
+        if !logged_in? || current_user.id != @user.id
+            flash[:danger] = "You can only edit your account."
+            redirect_to root_path
+        end
     end
 
 end
